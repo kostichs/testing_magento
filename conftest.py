b756b41522af26c85240ef082a427f3b73072b76
sergey.kostichev <sergey.kostichev@gcore.com>
2024-12-19 21:04:13 +0500

    installed allure and added headless mode

--- conftest.py
+++ conftest.py
@@ -1,20 +1,24 @@
 import pytest
+from allure_commons.types import AttachmentType
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from testing_magento.pages.sale_page import SalePage
 from testing_magento.pages.account_creation_page import CreationAccount
 from testing_magento.pages.collections_page import CollectionsPage
+import allure
 
 
 @pytest.fixture()
 def browser():
     options = Options()
     options.add_argument('--force-device-scale-factor=0.5')
+    options.add_argument('--headless')
     #  options.add_experimental_option('detach', True)
     browser = webdriver.Chrome(options=options)
     browser.maximize_window()
     browser.implicitly_wait(4)
-    return browser
+    yield browser
+    allure.attach(browser.get_screenshot_as_png(), name='Screenshot', attachment_type=AttachmentType.PNG)
 
 
 @pytest.fixture()
